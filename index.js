2ed9fb8b536945591ddc2325b733d4eac321f3db
bigacz <sbigacz@gmail.com>
2024-01-06 11:44:02 +0100

    add driver script

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 import Tree from './scripts/tree.js';
-import Node from './scripts/node.js';
+import { prettyPrint, getRandomNumbers } from './scripts/helperFunctions.js';
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-};
+const randomNumbers = getRandomNumbers();
+const testTree = new Tree(randomNumbers);
 
-const testNumbers = [
-  17, 14, 10, 18, 15, 8, 9, 2, 7, 4, 11, 19, 3, 5, 16, 6, 13, 1, 12, 20,
-];
-
-const testTree = new Tree(testNumbers);
-testTree.insert(20);
-testTree.insert(23);
-testTree.insert(24);
-testTree.insert(27);
-testTree.insert(25);
-testTree.insert(33);
-testTree.insert(54);
-prettyPrint(testTree.root);
 console.log(testTree.isBalanced());
-console.log(testTree.rebalance());
-console.log(testTree.isBalanced());
-prettyPrint(testTree.root);
 
-function logWithWorks(node) {
-  console.log(`${node.value} works`);
-}
+console.log(testTree.levelOrder());
+console.log(testTree.preOrder());
+console.log(testTree.postOrder());
+console.log(testTree.inOrder());
+
+testTree.insert(128);
+testTree.insert(143);
+testTree.insert(175);
+
+console.log(testTree.isBalanced());
+testTree.rebalance();
+console.log(testTree.isBalanced());
+
+console.log(testTree.levelOrder());
+console.log(testTree.preOrder());
+console.log(testTree.postOrder());
+console.log(testTree.inOrder());
+
+prettyPrint(testTree.root);
---
+++ scripts/helperFunctions.js
@@ -0,0 +1,28 @@
+function prettyPrint(node, prefix = '', isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
+
+function getRandomNumbers(amount = 10) {
+  const returnArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    const number = Math.round(Math.random() * 99);
+    returnArray.push(number);
+  }
+
+  return returnArray;
+}
+
+function logWithWorks(node) {
+  console.log(`${node.value} works`);
+}
+
+export { prettyPrint, getRandomNumbers };
